fe0a5fe5d05722f9261203ad0710e2a9f366a895
Zack Salama <zacksalama@gmail.com>
2024-10-01 17:29:11 -0400

    switched db to neon

--- src/django_backend/settings.py
+++ src/django_backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,19 +20,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_s&4z6zi%jp)@-!*xf)f(ls^zgu8^#&e%q+bhc*e_wtgc$wldi'
+SECRET_KEY = config("DJANGO_SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == 'true' or True
+DEBUG = config("DJANGO_DEBUG", cast=bool)
+
+#print("DEBUG", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
-    ".railway.app" #https://saas.prod.railway.app
+    ".railway.app",
+    ".bookmethisasap.com" #https://saas.prod.railway.app
 ]
 if DEBUG:
     ALLOWED_HOSTS += [
         "127.0.0.1",
         "localhost",
-        ".bookmethisasap.com"
     ]
 
 
@@ -89,6 +91,19 @@ DATABASES = {
     }
 }
 
+CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
+DATABASE_URL = config("DATABASE_URL", cast=str)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=CONN_MAX_AGE,
+            conn_health_checks=True,
+            )
+    
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
